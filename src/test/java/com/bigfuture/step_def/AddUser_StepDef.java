5b25cee342deff52ef192ed5b18c32f8dbdc06a1
kdrsyl <kadersayligb@gmail.com>
2020-11-12 04:24:08 +0000

    adduser stepdef edited

--- src/test/java/com/bigfuture/step_def/AddUser_StepDef.java
+++ src/test/java/com/bigfuture/step_def/AddUser_StepDef.java
@@ -11,12 +11,19 @@ import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
+import java.util.Map;
 
 public class AddUser_StepDef {
 
     LoginPage loginPage = new LoginPage();
     UsersPage usersPage = new UsersPage();
+
 
     @Given("the user logged in as a {string}")
     public void theUserLoggedInAsA(String librarian) {
@@ -37,53 +44,77 @@ public class AddUser_StepDef {
     }
 
 
-    @Then("click the {string} button")
-    public void clickTheButton(String addUser) {
+    @Given("user clicks the {string} button")
+    public void userClicksTheButton(String addUser) {
 
         usersPage.addUser.click();
-        BrowserUtils.waitFor(2);
-
-        usersPage.close.click();
-
     }
 
-    @And("create new user using following information")
-    public void createNewUserUsingFollowingInformation() {
+    @When("create new user using following information")
+    public void createNewUserUsingFollowingInformation(Map<String,String> form) {
 
-        BrowserUtils.waitFor(3);
-        usersPage.fullName.sendKeys("Sarah White");
-        usersPage.password1.sendKeys("1234");
-        usersPage.email.sendKeys("shwhiter@gmail.com");
+        Select dropdown1 = new Select(usersPage.dropdown1);
+        Select dropdown2 = new Select(usersPage.dropdown2);
 
-        BrowserUtils.waitFor(2);
-        Select userGroup = new Select(usersPage.dropdown1);
-        userGroup.selectByValue("3");
+        usersPage.fullName.sendKeys(form.get(("Full Name")));
+        usersPage.password1.sendKeys(form.get("Password"));
+        usersPage.email.sendKeys(form.get("Email"));
 
-        BrowserUtils.waitFor(2);
-        Select status = new Select(usersPage.dropdown2);
-        status.selectByValue("ACTIVE");
+        dropdown1.selectByValue(form.get("User Group"));
+        dropdown2.selectByValue(form.get("Status"));
 
-
-        BrowserUtils.waitFor(1);
         usersPage.startDate.clear();
-        BrowserUtils.waitFor(1);
-        usersPage.endDate.clear();
-        BrowserUtils.waitFor(1);
-        usersPage.startDate.sendKeys("2020-11-11");
-        BrowserUtils.waitFor(1);
-        usersPage.endDate.sendKeys("2025-10-10");
+        usersPage.startDate.sendKeys(form.get("Start Date"));
 
-        usersPage.address.sendKeys("Spring st, New York, USA, 20002");
+        usersPage.endDate.clear();
+        usersPage.endDate.sendKeys(form.get("End Date"));
+
+        usersPage.address.sendKeys(form.get("Address"));
+
+    }
+    @Then("user able to click the {string} button")
+    public void userAbleToClickTheButton(String save) {
         usersPage.save.click();
+        BrowserUtils.waitFor(2);
+    }
+
+    @When("librarian clicks the {string} button")
+    public void librarianClicksTheButton(String addUser) {
+
+        usersPage.addUser.click();
+    }
+
+    @Then("librarian click the {string} button")
+    public void librarianClickTheButton(String close) {
+        BrowserUtils.waitFor(2);
+        usersPage.close.click();
+    }
+
+    @And("librarian able to click the {string} button")
+    public void librarianAbleToClickTheButton(String editUser) {
+        usersPage.edit.click();
+        BrowserUtils.waitFor(2);
 
     }
 
     @And("edit user info using following information")
-    public void editUserInfoUsingFollowingInformation() {
+    public void editUserInfoUsingFollowingInformation(Map<String,String> edit) {
 
+        usersPage.fullName.clear();
+        usersPage.fullName.sendKeys(edit.get(("Full Name")));
+        BrowserUtils.waitFor(2);
 
+        usersPage.password1.clear();
+        usersPage.password1.sendKeys(edit.get("Password"));
+        BrowserUtils.waitFor(2);
 
+        usersPage.email.clear();
+        usersPage.email.sendKeys(edit.get("Email"));
     }
 
-
+    @Then("librarian click {string} button")
+    public void librarianClickButton(String save) {
+        usersPage.save.click();
+        BrowserUtils.waitFor(2);
+    }
 }
